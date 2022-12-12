9c1a2cf4498985f838abf0aff73eac88a8d9493f
RishiDarkDevil <rishi8001100192@gmail.com>
2022-12-12 08:07:22 +0530

    Add circle markers on click

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ map_ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   
   # Map
-  leafletOutput("map", width = "100%", height = "100%"),
+  leafletOutput("map", width = "100%", height = "100%")
 )
 
 server <- function(input, output, session) {
@@ -18,6 +18,14 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles()
+  })
+  
+  # Add Circle Markers on clicks on the Map
+  observeEvent(input$map_click, {
+    click = input$map_click
+    
+    leafletProxy('map') %>%
+      addCircleMarkers(lng = click$lng, lat = click$lat)
   })
 }
 
